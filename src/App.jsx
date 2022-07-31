13e78b1502841cb21ca373b0995813e524b22a59
apreynolds1989 <70614076+apreynolds1989@users.noreply.github.com>
2022-07-30 23:43:00 -0230

    created Routing component and moved all Routes from App to Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { gtTheme } from './assets/theme';
-import { Landing } from './pages/Landing';
 import './index.css';
 import { CssBaseline, useMediaQuery } from '@mui/material';
-import { HeaderAppBar } from './components/HeaderAppBar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Skaters } from './pages/Skaters';
-import { Footer } from './components/Footer';
-import { Goalies } from './pages/Goalies';
-import { Schedule } from './pages/Schedule';
-import { About } from './pages/About';
+import { Routing } from './Routing';
 
 export const App = () => {
     const isMobileSize = useMediaQuery('(max-width:600px)');
@@ -18,17 +11,7 @@ export const App = () => {
     return (
         <ThemeProvider theme={gtTheme}>
             <CssBaseline />
-            <BrowserRouter>
-                <HeaderAppBar isMobileSize={isMobileSize} />
-                <Routes>
-                    <Route path='/nextUP/' element={<Landing />} />
-                    <Route path='nextUP/skaters' element={<Skaters />} />
-                    <Route path='nextUP/goalies' element={<Goalies />} />
-                    <Route path='nextUP/schedule' element={<Schedule />} />
-                    <Route path='nextUP/about' element={<About />} />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
+            <Routing isMobileSize={isMobileSize} />
         </ThemeProvider>
     );
 };
---
+++ src/Routing.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { HeaderAppBar } from './components/HeaderAppBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Landing } from './pages/Landing';
+import { Skaters } from './pages/Skaters';
+import { Footer } from './components/Footer';
+import { Goalies } from './pages/Goalies';
+import { Schedule } from './pages/Schedule';
+import { About } from './pages/About';
+
+export const Routing = ({ isMobileSize }) => {
+    return (
+        <BrowserRouter>
+            <HeaderAppBar isMobileSize={isMobileSize} />
+            <Routes>
+                <Route path='/nextUP/' element={<Landing />} />
+                <Route path='nextUP/skaters' element={<Skaters />} />
+                <Route path='nextUP/goalies' element={<Goalies />} />
+                <Route path='nextUP/schedule' element={<Schedule />} />
+                <Route path='nextUP/about' element={<About />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
+};
