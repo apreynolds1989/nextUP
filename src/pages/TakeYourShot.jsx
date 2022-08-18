a62797eaec80f1c27b9658969ae7c64cd92a731f
apreynolds1989 <70614076+apreynolds1989@users.noreply.github.com>
2022-08-18 00:24:29 -0230

    Updated pucks in TakeYourShot page

--- src/pages/TakeYourShot.jsx
+++ src/pages/TakeYourShot.jsx
@@ -9,9 +9,12 @@ import {
 } from '@mui/material';
 import { NextUpLogo } from '../components/NextUpLogo';
 import { SplashBackground } from '../components/SplashBackground';
-import { SkatersPuck } from '../components/SkatersPuck';
-import { GoaliesPuck } from '../components/GoaliesPuck';
-import { SchedulePuck } from '../components/SchedulePuck';
+import PlayersPuck from '../assets/img/PlayersPuck.png';
+import SchedulePuck from '../assets/img/SchedulePuck.png';
+import AboutPuck from '../assets/img/AboutPuck.png';
+// import { SkatersPuck } from '../components/SkatersPuck';
+// import { GoaliesPuck } from '../components/GoaliesPuck';
+// import { SchedulePuck } from '../components/SchedulePuck';
 import { Link } from 'react-router-dom';
 import { landingTheme } from '../assets/theme';
 
@@ -56,7 +59,12 @@ export const TakeYourShot = () => {
                         }}
                     >
                         <Link to='/nextUP/'>
-                            <SkatersPuck />
+                            <img
+                                src={PlayersPuck}
+                                alt='Puck that says players'
+                                width={200}
+                            />
+                            {/* <SkatersPuck /> */}
                         </Link>
                     </Button>
                 </Grid>
@@ -67,7 +75,12 @@ export const TakeYourShot = () => {
                         }}
                     >
                         <Link to='/nextUP/schedule'>
-                            <SchedulePuck />
+                            <img
+                                src={SchedulePuck}
+                                alt='Puck that says schedule'
+                                width={200}
+                            />
+                            {/* <SchedulePuck /> */}
                         </Link>
                     </Button>
                 </Grid>
@@ -78,7 +91,12 @@ export const TakeYourShot = () => {
                         }}
                     >
                         <Link to='/nextUP/about'>
-                            <GoaliesPuck />
+                            <img
+                                src={AboutPuck}
+                                alt='Puck that says about'
+                                width={200}
+                            />
+                            {/* <GoaliesPuck /> */}
                         </Link>
                     </Button>
                 </Grid>
