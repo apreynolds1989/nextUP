315bcb3bbd411515aa91faa51ce9efb173d419df
apreynolds1989 <70614076+apreynolds1989@users.noreply.github.com>
2022-08-20 00:28:29 -0230

    Converted GameDayBox to a card

--- src/components/GameDayBox.jsx
+++ src/components/GameDayBox.jsx
@@ -1,48 +1,69 @@
-import { Box, List, Typography, ListItem, ListItemText } from '@mui/material';
+import {
+    Box,
+    List,
+    Typography,
+    ListItem,
+    ListItemText,
+    Card,
+    CardContent,
+} from '@mui/material';
 import React from 'react';
 import { weeklyGames } from '../assets/data/games';
-import { palette } from '../assets/theme';
+// import { palette } from '../assets/theme';
 
 export const GameDayBox = ({ day }) => {
     const gamesArr = weeklyGames[day];
 
     return (
-        <Box
+        <Card
             sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                borderBottom: `2px solid ${palette.gtBlue}`,
+                borderRadius: 5,
+                marginY: 3,
             }}
         >
-            <Box>
-                <Typography
-                    variant='h5'
+            <CardContent>
+                <Box
                     sx={{
-                        textAlign: 'center',
-                        textDecoration: 'underline',
-                        textUnderlineOffset: '5px',
-                        paddingTop: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
                     }}
                 >
-                    {day}
-                </Typography>
-            </Box>
-            <Box>
-                <List>
-                    {gamesArr.map((game, index) => (
-                        <ListItem
-                            key={`${day} Game ${index + 1}`}
+                    <Box>
+                        <Typography
+                            variant='h5'
                             sx={{
                                 textAlign: 'center',
+                                textDecoration: 'underline',
+                                textUnderlineOffset: '5px',
+                                paddingTop: 1,
                             }}
                         >
-                            <ListItemText
-                                primary={`Game ${index + 1} - ${game}`}
-                            />
-                        </ListItem>
-                    ))}
-                </List>
-            </Box>
-        </Box>
+                            {day}
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <List>
+                            {gamesArr.map((game, index) => (
+                                <ListItem
+                                    key={`${day} Game ${index + 1}`}
+                                    sx={{
+                                        textAlign: 'center',
+                                        paddingY: 0,
+                                        fontSize: '12px',
+                                    }}
+                                >
+                                    <ListItemText
+                                        primary={`Game ${index + 1} - ${game}`}
+                                        primaryTypographyProps={{
+                                            fontSize: '14px',
+                                        }}
+                                    />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Box>
+                </Box>
+            </CardContent>
+        </Card>
     );
 };
