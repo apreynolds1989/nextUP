6809bf9dadfa7941f16f0b024fdb0ceddb026f1e
apreynolds1989 <70614076+apreynolds1989@users.noreply.github.com>
2022-09-28 02:19:41 -0230

    Moved Pagination to it's own component

--- src/components/customTableComponents/DoubleSortTable.jsx
+++ src/components/customTableComponents/DoubleSortTable.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { Box, Button, useMediaQuery } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Box, useMediaQuery } from '@mui/material';
 import { DoubleSortTableRows } from './DoubleSortTableRows';
 import { DoubleSortTableHeader } from './DoubleSortTableHeader';
 import { TableFilterModal } from './TableFilterModal';
 import { sortPrimary } from './utilities/sortFunctions';
-import { useEffect } from 'react';
+import { Pagination } from './Pagination';
 
 export const DoubleSortTable = ({
     columnConfig,
@@ -71,40 +71,6 @@ export const DoubleSortTable = ({
         }
     }, [rowOrder, firstItemOnPage, lastItemOnPage]);
 
-    const paginationNextHandler = () => {
-        setFirstItemOnPage(firstItemOnPage + 25);
-        setLastItemOnPage(
-            lastItemOnPage + 25 > rowOrder.length
-                ? rowOrder.length
-                : lastItemOnPage + 25,
-        );
-        setPaginatedRows(rowOrder.slice(firstItemOnPage, lastItemOnPage));
-    };
-
-    const paginationPrevHandler = () => {
-        const lastItemOfArr = rowOrder.length - 1;
-        setFirstItemOnPage(firstItemOnPage - 25 < 0 ? 0 : firstItemOnPage - 25);
-        setLastItemOnPage(
-            lastItemOnPage === rowOrder.length
-                ? lastItemOfArr - (rowOrder.length % 25)
-                : lastItemOnPage - 25,
-        );
-        setPaginatedRows(rowOrder.slice(firstItemOnPage, lastItemOnPage));
-    };
-
-    const paginationFirstPageHandler = () => {
-        setFirstItemOnPage(0);
-        setLastItemOnPage(25);
-        setPaginatedRows(rowOrder.slice(firstItemOnPage, lastItemOnPage));
-    };
-
-    const paginationLastPageHandler = () => {
-        const lastItemOfArr = rowOrder.length - 1;
-        setFirstItemOnPage(lastItemOfArr - (rowOrder.length % 25));
-        setLastItemOnPage(lastItemOfArr + 1);
-        setPaginatedRows(rowOrder.slice(firstItemOnPage, lastItemOnPage));
-    };
-
     return (
         <>
             {isFilterable && (
@@ -159,67 +125,15 @@ export const DoubleSortTable = ({
                 </Box>
             </Box>
             {paginatedRows.length > 0 && (
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                        marginTop: 2,
-                    }}
-                >
-                    <Button
-                        onClick={
-                            firstItemOnPage !== 0
-                                ? paginationFirstPageHandler
-                                : null
-                        }
-                        sx={{
-                            fontWeight: 'bold',
-                            borderRight: 2,
-                            borderColor: renderedProps.dataTextColor,
-                            borderRadius: 0,
-                        }}
-                    >
-                        First Page
-                    </Button>
-                    <Button
-                        onClick={
-                            firstItemOnPage !== 0 ? paginationPrevHandler : null
-                        }
-                        sx={{
-                            fontWeight: 'bold',
-                            borderRight: 2,
-                            borderColor: renderedProps.dataTextColor,
-                            borderRadius: 0,
-                        }}
-                    >
-                        Prev 25
-                    </Button>
-                    <Button
-                        onClick={
-                            lastItemOnPage !== rowOrder.length
-                                ? paginationNextHandler
-                                : null
-                        }
-                        sx={{
-                            fontWeight: 'bold',
-                            borderRight: 2,
-                            borderColor: renderedProps.dataTextColor,
-                            borderRadius: 0,
-                        }}
-                    >
-                        Next 25
-                    </Button>
-                    <Button
-                        onClick={
-                            lastItemOnPage !== rowOrder.length
-                                ? paginationLastPageHandler
-                                : null
-                        }
-                        sx={{ fontWeight: 'bold' }}
-                    >
-                        Last Page
-                    </Button>
-                </Box>
+                <Pagination
+                    firstItemOnPage={firstItemOnPage}
+                    setFirstItemOnPage={setFirstItemOnPage}
+                    lastItemOnPage={lastItemOnPage}
+                    setLastItemOnPage={setLastItemOnPage}
+                    setPaginatedRows={setPaginatedRows}
+                    rowOrder={rowOrder}
+                    renderedProps={renderedProps}
+                />
             )}
         </>
     );
